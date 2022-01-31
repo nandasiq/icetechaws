208e696d1f4883bb8c3fe61fe29405c1c60b08c6
Fernanda Siqueira <nandasiq@gmail.com>
2022-01-31 13:44:12 -0300

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "icetech": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "icetech705ff910": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function header = React.create() => {
+    <div>
+        <h1> ICETECH </h1>
+        <h5>Manutenção em ar condicionado</h5>
+    </div>
+}
+
+export default header;
